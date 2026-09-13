dfe22c049ed6307a2b2a56890547ef055176ef18
viv-climber <julionico2009@gmail.com>
2026-09-13 10:36:09 +0200

    add a class in which you learn about learning

---
+++ theory.py
@@ -0,0 +1,24 @@
+print("Hello there! Here, you will learn the theory of learning." \
+" In order to make this more enjoyale, you will be answering " \
+"some questions regarding learning. I warn you that this class" \
+"will be very related with neuroplasticity")
+
+answer1 = input("What means learning for you? :")
+
+print("For us, learning is the physical changes that happen in" \
+"yout brain. These can be:" \
+"1 from the gain of new information" \
+"2 From the strenghtening or weakening of some circuit, so things get easier and faster" \
+"or harder and slower (this is very related with new skills or " \
+"forgetting skills adquired")
+
+answer2 = input("How you think we can optimize our learning so" \
+"our brain actually changes? :")
+
+print("The actual best way of securing these sypnatic connections " \
+"(or neuronal connections) is to actually trigger them." \
+"To basically activate them, if for example you've learned new" \
+"information, this would mean to test on you on that information. " \
+"If otherwise you've learned a skill as for example how to use" \
+"a function in python, in order to trigger this, you would have" \
+"to implement it.")
